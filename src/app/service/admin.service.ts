4b6df99800bb6e12a7aa38d84f99f307afabe6cf
Sunying RONG <sunying.rong@hotmail.com>
2024-02-18 21:29:53 -0500

    change to local connection

--- src/app/service/admin.service.ts
+++ src/app/service/admin.service.ts
@@ -7,8 +7,7 @@ import { Admin } from '../model/admin.model';
   providedIn: 'root'
 })
 export class AdminService {
-  // private baseUrl = "http://localhost:8080/api";
-  private baseUrl = "https://java-avance-backend.agreeablebush-b953dea1.eastus.azurecontainerapps.io/api";
+  private baseUrl = "http://localhost:8080/api";
 
   constructor(private http: HttpClient) { }
 
